53a31cf6fad26135e61f623b295da84a34a2f552
ParvinYagubzada <parvinyy@code.edu.az>
2021-07-16 18:07:23 +0400

    Initial commit

---
+++ src/main/java/az/code/tourapi/TourApiApplication.java
@@ -0,0 +1,13 @@
+package az.code.tourapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourApiApplication.class, args);
+    }
+
+}
